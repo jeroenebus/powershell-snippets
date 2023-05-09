ec0117c516c8c068548fb9bf8d6223f50c2e96f2
Jeroen Ebus <45665324+jeroenebus@users.noreply.github.com>
2023-05-09 19:26:31 +0200

    Create Fix-Expand-Shell-Folder.ps1
    Prereq for successful installation when the following error occurs:
    Internal error: Failed to expand shell folder constant "userdocs"
    Created: "C:\Windows\SysWOW64\config\systemprofile\Documents" manually to bypass the installation error.

---
+++ Fix-Expand-Shell-Folder.ps1
@@ -0,0 +1,17 @@
+<#
+.Synopsis
+   Quick fix for expand shell folder contant error
+.DESCRIPTION
+   Prereq for succesfull installation when the following error occurs:
+   Internal error: Failed to expand shell folder constant "userdocs"
+   Created "C:\Windows\SysWOW64\config\systemprofile\Documents" manually to bypass the installation error.
+.EXAMPLE
+   Could also be used for the error "userdocuments", just replace below
+.NOTES
+    Filename: Fix-Expand-Shell-Folder.ps1
+    Author: Jeroen Ebus (https://manage-the.cloud) 
+    Modified date: 2023-05-09
+    Version 1.0 - Release notes/details
+#>
+
+mkdir "C:\Windows\SysWOW64\config\systemprofile\Desktop"
